856531cff31c847268b73ed988f8251e0d565c45
68010417 <68010417@kmitl.ac.th>
2025-12-15 10:27:51 +0700

    all10

--- src/q01/grade.ts
+++ src/q01/grade.ts
@@ -1,27 +1,24 @@
 const input = process.argv[2];
+const score = Number(input);
 
-if(!input || input.trim() === ''){
+if(process.argv[2] === ""){
     console.log("Invalid Input");
-} else {
-    const score = Number(input);
-    
-    if(isNaN(score) || score > 100 || score < 0){
-        console.log("Invalid Input");
-    } else {
-        if(score >= 80){
-            console.log("Grade is A");
-        }
-        else if(score >= 70){
-            console.log("Grade is B");
-        }
-        else if(score >= 60){
-            console.log("Grade is C");
-        }
-        else if(score >= 50){
-            console.log("Grade is D");
-        }
-        else {
-            console.log("Grade is F");
-        }
+}else if(isNaN(score) || score > 100 || score < 0  ){
+    console.log("Invalid Input");
+}else{
+    if(score >= 80){
+        console.log("Grade is A");
+    }
+    else if(score >= 70){
+        console.log("Grade is B");
+    }
+    else if(score >= 60){
+        console.log("Grade is C");
+    }
+    else if(score >= 50){
+        console.log("Grade is D");
+    }
+    else if(score <= 50){
+        console.log("Grade is F");
     }
 }
