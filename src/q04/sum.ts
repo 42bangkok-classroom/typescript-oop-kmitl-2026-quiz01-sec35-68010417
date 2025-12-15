b1270bc058eb7175e044e60e7cb58327693b6a61
68010417 <68010417@kmitl.ac.th>
2025-12-15 10:03:16 +0700

    all2

---
+++ src/q04/sum.ts
@@ -0,0 +1,13 @@
+const input = process.argv[2];
+const number = Number(input);
+let sum :number = 0;
+
+
+if(isNaN(number)|| number < 1 || !number){
+    console.log("Invalid Input");
+}else{
+    for(let i=0; i <= number;i++){
+        sum += i;
+    };
+    console.log(sum);
+}
