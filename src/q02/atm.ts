6f13e5b02d55c65d347697ea89378bd9f7fff05d
68010417 <68010417@kmitl.ac.th>
2025-12-15 09:42:09 +0700

    q02

---
+++ src/q02/atm.ts
@@ -0,0 +1,17 @@
+const input = process.argv[2];
+const balance = Number(input);
+
+const input_2 = process.argv[3];
+const withdraw = Number(input_2);
+
+if(isNaN(balance) || isNaN(withdraw)){
+    console.log("Invalid Input");
+}else{
+    if(withdraw > balance){
+        console.log("Insufficient balance");
+    }else if(withdraw > 5000){
+        console.log("Exceeds per-withdrawal limit");
+    }else{
+        console.log("Withdrawal approved");
+    }
+}
