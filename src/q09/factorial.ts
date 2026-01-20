ba2ed8733f7fdfcd83c66088ea083ac3ef0108ba
68010417 <68010417@kmitl.ac.th>
2026-01-20 18:36:16 +0700

    q09_2

--- src/q09/factorial.ts
+++ src/q09/factorial.ts
@@ -1,12 +1,12 @@
 const input = process.argv[2];
 const n = Number(input);
 
-if(isNaN(n)|| n < 1 || input === "" || !Number.isInteger(n)){
+if(isNaN(n)|| n < 0 || input === "" || !Number.isInteger(n) || input.trim() === ""){
     console.log("Invalid Input")
 }else{
     let result = 1;
     for(let i = 1;i<=n;i++){
         result *= i;
     }
-    console.log(result);
+    console.log(`${result}`);
 }
